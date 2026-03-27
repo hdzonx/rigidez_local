288474bd6b6eeaf00ad0e12bce62f3d0d2426d94
Hudson <hudson.creisp@gmail.com>
2026-03-27 16:44:57 -0300

    testes na montagem de k global passou

--- src/tests.rs
+++ src/tests.rs
@@ -21,7 +21,7 @@ mod tests {
     //use approx::relative_eq;
     use nalgebra::Matrix3;
     use nalgebra::Matrix6;
-    use nalgebra::MatrixN;
+    use nalgebra::SMatrix;
 
     use crate::area_triangulo;
     use crate::constitutive_matrix;
@@ -38,13 +38,13 @@ mod tests {
 
         let escalar = 3.297e6;
 
-        let matriz_inicial: Matrix6<f64> = Matrix6::new(
+        let matriz_nua: Matrix6<f64> = Matrix6::new(
             0.5, 0.0, -0.75, 0.15, 0.25, -0.15, 0., 0.175, 0.175, -0.2627, -0.175, 0.088, -0.75,
             0.1750, 1.3, -0.488, -0.55, 0.313, 0.15, -0.2627, -0.488, 0.894, 0.338, -0.631, 0.25,
             -0.175, -0.55, 0.338, 0.3, -0.163, -0.15, 0.088, 0.313, -0.631, -0.163, 0.544,
         );
 
-        let rigidez_local_esperada = escalar * matriz_inicial;
+        let rigidez_local_esperada = escalar * matriz_nua;
 
         let poisson = 0.3;
         let elasticidade = 30.0e6;
@@ -86,13 +86,13 @@ mod tests {
 
         let escalar = 3.297e6;
 
-        let matriz_inicial: Matrix6<f64> = Matrix6::new(
+        let matriz_nua: Matrix6<f64> = Matrix6::new(
             0.15, 0.081, -0.25, -0.175, 0.1, 0.094, 0.081, 0.272, -0.15, -0.088, 0.069, -0.184,
             -0.25, -0.15, 1.0, 0.0, -0.75, 0.15, -0.175, -0.088, 0.0, 0.35, 0.175, -0.263, 0.1,
             0.069, -0.75, 0.175, 0.65, -0.244, 0.094, -0.184, 0.15, -0.263, -0.244, 0.447,
         );
 
-        let rigidez_local_esperada = escalar * matriz_inicial;
+        let rigidez_local_esperada = escalar * matriz_nua;
 
         let poisson = 0.3;
         let elasticidade = 30.0e6;
@@ -128,6 +128,15 @@ mod tests {
     fn test_rigidez_global_esparsa() {
         //Matriz de rigidez global esperada
         let escalar = 3.297e6;
+        let matriz_nua = SMatrix::<f64, 8, 8>::from_row_slice(&[
+            0.65, 0.081, -0.75, 0.15, 0.0, -0.325, 0.1, 0.094, 0.081, 0.447, 0.175, -0.263, -0.325,
+            0.0, 0.069, -0.184, -0.75, 0.175, 1.3, -0.488, -0.55, 0.313, 0.0, 0.0, 0.15, -0.263,
+            -0.488, 0.894, 0.338, -0.631, 0.0, 0.0, 0.0, -0.325, -0.55, 0.338, 1.3, -0.163, -0.75,
+            0.15, -0.325, 0.0, 0.313, -0.631, -0.163, 0.894, 0.175, -0.263, 0.1, 0.069, 0.0, 0.0,
+            -0.75, 0.175, 0.65, -0.244, 0.094, -0.184, 0.0, 0.0, 0.15, -0.263, -0.244, 0.447,
+        ]);
+
+        let rigidez_global_esperada = escalar * matriz_nua;
 
         //Matriz constitutiva
         let espessura = 0.1;
@@ -170,7 +179,26 @@ mod tests {
         println!("Matriz global (CSR):");
         println!("{:?}", k_global);
         println!("\nConvertendo para matriz densa para visualização:\n");
-        let dense = k_global.to_dense();
-        println!("{}", dense);
+        let dense_global_k = k_global.to_dense();
+        println!("{}", dense_global_k);
+
+        //Avalie o erro com assertion para cada valor da matriz
+        for i in 0..rigidez_global_esperada.nrows() {
+            for j in 0..rigidez_global_esperada.ncols() {
+                let tol = 1e4;
+                println!("i = {}, j ={}", i + 1, j + 1);
+                let relat_err = (rigidez_global_esperada[(i, j)] - dense_global_k[(i, j)]).abs();
+
+                println!("calculado = {}", dense_global_k[(i, j)]);
+                println!("esperado = {}", rigidez_global_esperada[(i, j)]);
+
+                assert!(
+                    relat_err < tol,
+                    "Erro relativo alto demais: {} (esperado < {})",
+                    relat_err,
+                    tol
+                );
+            }
+        }
     }
 }
