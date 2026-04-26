b1a4e7fc2963623a30b66b07c6c678a5edbcf05e
Hudson <hudson.creisp@gmail.com>
2026-04-25 21:49:05 -0300

    em test2 agrupamento ordenado dos nós

--- src/test2.rs
+++ src/test2.rs
@@ -34,46 +34,80 @@ mod test {
     fn chapa() {
         //Propriedades de material
         let poisson = 0.3;
-        let elasticity = 30.0e6;
+        let elasticity = 1.0;
         let espessura = 1.0;
-        //Elemento 1
-        let x_coords_el_1 = vec![0.0, 0.0, 5.0];
-        let y_coords_el_1 = vec![10.0, 5.0, 5.0];
+        //Elemento 1: Nós [4, 5, 1]
+        let x_coords_el_1 = vec![0.0, 5.0, 0.0];
+        let y_coords_el_1 = vec![5.0, 5.0, 10.0];
 
-        //Elemento 2
-        let x_coords_el_2 = vec![0.0, 5.0, 5.0];
-        let y_coords_el_2 = vec![10.0, 5.0, 10.0];
+        //Elemento 2 Nós [5, 2, 1]
+        let x_coords_el_2 = vec![5.0, 5.0, 0.0];
+        let y_coords_el_2 = vec![5.0, 10.0, 10.0];
 
-        //Elemento 3
-        let x_coords_el_3 = vec![5.0, 5.0, 10.0];
-        let y_coords_el_3 = vec![10.0, 5.0, 5.0];
+        //Elemento 3: Nós [5,6,2]
+        let x_coords_el_3 = vec![5.0, 10.0, 5.0];
+        let y_coords_el_3 = vec![5.0, 5.0, 10.0];
 
-        //Elemento 4
+        //Elemento 4: Nós [2,6,3]
         let x_coords_el_4 = vec![5.0, 10.0, 10.0];
         let y_coords_el_4 = vec![10.0, 5.0, 10.0];
 
-        //Elemento 5
+        //Elemento 5: Nós [4,8,5]
         let x_coords_el_5 = vec![0.0, 5.0, 5.0];
         let y_coords_el_5 = vec![5.0, 0.0, 5.0];
 
-        //Elemento 6
+        //Elemento 6: Nós [4,7,8]
         let x_coords_el_6 = vec![0.0, 0.0, 5.0];
         let y_coords_el_6 = vec![5.0, 0.0, 0.0];
 
-        //Elemento 7
+        //Elemento 7: Nós [5,8,9]
         let x_coords_el_7 = vec![5.0, 5.0, 10.0];
         let y_coords_el_7 = vec![5.0, 0.0, 0.0];
 
-        //Elemento 8
+        //Elemento 8: Nós [9,6,5]
         let x_coords_el_8 = vec![10.0, 10.0, 5.0];
         let y_coords_el_8 = vec![0.0, 5.0, 5.0];
 
+        //Matriz constitutiva
         let constitutive_matrix =
             constitutive_matrix::constitutive_matrix("Plane stress", poisson, elasticity);
 
+        //Rigidez do elemento 1
         let rigidez_local_el_1 =
             matriz_rigidez_local(x_coords_el_1, y_coords_el_1, espessura, constitutive_matrix);
+        //Rigidez do elemento 2
+        let rigidez_local_el_2 =
+            matriz_rigidez_local(x_coords_el_2, y_coords_el_2, espessura, constitutive_matrix);
+        //Rigidez do elemento 3
+        let rigidez_local_el_3 =
+            matriz_rigidez_local(x_coords_el_3, y_coords_el_3, espessura, constitutive_matrix);
+        //Rigidez do elemento 4
+        let rigidez_local_el_4 =
+            matriz_rigidez_local(x_coords_el_4, y_coords_el_4, espessura, constitutive_matrix);
+        //Rigidez do elemento 5
+        let rigidez_local_el_5 =
+            matriz_rigidez_local(x_coords_el_5, y_coords_el_5, espessura, constitutive_matrix);
+        //Rigidez do elemento 6
+        let rigidez_local_el_6 =
+            matriz_rigidez_local(x_coords_el_6, y_coords_el_6, espessura, constitutive_matrix);
+        //Rigidez do elemento 7
+        let rigidez_local_el_7 =
+            matriz_rigidez_local(x_coords_el_7, y_coords_el_7, espessura, constitutive_matrix);
+        //Rigidez do elemento 8
+        let rigidez_local_el_8 =
+            matriz_rigidez_local(x_coords_el_8, y_coords_el_8, espessura, constitutive_matrix);
 
-        println!("rigidez do elemento 1: {:?}", rigidez_local_el_1);
+        //Mapeamento de cada nó em cada elemento.
+        //O índice i = nó-1
+        let elements = vec![
+            ([3, 4, 0], rigidez_local_el_1),
+            ([4, 1, 0], rigidez_local_el_2),
+            ([4, 5, 1], rigidez_local_el_3),
+            ([1, 5, 2], rigidez_local_el_4),
+            ([3, 7, 4], rigidez_local_el_5),
+            ([3, 6, 7], rigidez_local_el_6),
+            ([4, 7, 8], rigidez_local_el_7),
+            ([8, 5, 4], rigidez_local_el_8),
+        ];
     }
 }
